7ad083d6ae27ae420275b359043f96216935b02e
Lynxito <nabil.lmoutaouakil@gmail.com>
2024-10-23 16:56:37 +0200

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #!/bin/bash
-gcc
-#include<stdio.h>
-int main (void) {puts("programming is like multilingual puzzle):
-	return 0:
-}
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+print(f"Programming is like building a multilingual puzzle)
+return 0:
